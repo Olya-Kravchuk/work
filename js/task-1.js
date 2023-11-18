156247946e27d4bf9608d3145e61401fddb62808
Olya-Kravchuk <143799228+Olya-Kravchuk@users.noreply.github.com>
2023-11-18 14:43:41 +0100

    task-1

---
+++ js/task-1.js
@@ -0,0 +1,21 @@
+//  Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
+
+// function checkA(a) {
+//   if (a === 0) {
+//     return "Вірно";
+//   } 
+//   return "Невірно";
+// }
+// console.log(checkA(10));
+// console.log(checkA(3));
+
+// function checkA(a) {
+//   if (a === 10) {
+//     return "Вірно";
+//   } 
+//   return a === 10 ? "Вірно" : "Невірно";
+// }
+// console.log(checkA(10));
+// console.log(checkA(3)); 
+
+
